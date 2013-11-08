b106500987401606e2a560f024bfbfa595eaa1ad
amarie4224 <astanley@go-optimum.com>
2013-11-07 17:20:10 -0800

    Evening Commit

---
+++ js/wembli-specific.js
@@ -0,0 +1,11 @@
+$('.parent').click(function() {
+    var subMenu = $(this).siblings('ul');
+    if ($(subMenu).hasClass('open')) {
+        $(subMenu).fadeOut();
+        $(subMenu).removeClass('open').addClass('closed');
+    }
+    else {
+        $(subMenu).fadeIn();
+        $(subMenu).removeClass('closed').addClass('open');
+    }
+});
